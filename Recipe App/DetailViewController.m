3a5f8f1da89efc7702af880e5ed996dd47dfe5ac
christianmonson <christianrmonson@gmail.com>
2015-02-17 16:31:10 -0700

    completes project
    Signed-off-by: christianmonson <christianrmonson@gmail.com>

--- Recipe App/DetailViewController.m
+++ Recipe App/DetailViewController.m
@@ -15,7 +15,7 @@ static NSString * const ingredientKey = @"ingredientType";;
 static NSString * const ingredientVolumeKey = @"ingredientVolume";
 @interface DetailViewController ()
 
-@property (nonatomic, )  CGFloat lengthOfPreviousFeild;
+@property (nonatomic, assign) CGFloat lengthOfPreviousField;
 @property (nonatomic, strong) UIScrollView *scrollView;
 @property (nonatomic, strong) RecipesTableViewDataSource *dataSource;
 @end
@@ -27,7 +27,7 @@ static NSString * const ingredientVolumeKey = @"ingredientVolume";
     
     CGFloat widthWithMargin = self.view.frame.size.width - (2 * margin);
     
-    self.lengthOfPreviousFeild = margin;
+    self.lengthOfPreviousField = margin;
     
     self.scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
     [self.view addSubview:self.scrollView];
@@ -39,21 +39,21 @@ static NSString * const ingredientVolumeKey = @"ingredientVolume";
     
 //description
     
-    UILabel *description = [[UILabel alloc] initWithFrame:CGRectMake(margin,self.lengthOfPreviousFeild,widthWithMargin,100)];
+    UILabel *description = [[UILabel alloc] initWithFrame:CGRectMake(margin,self.lengthOfPreviousField,widthWithMargin,100)];
     description.text = [RARecipes descriptionAtIndex:self.indexPathSelected.row];
     description.numberOfLines=0;
     [description sizeToFit];
-    self.lengthOfPreviousFeild += description.frame.size.height;
+    self.lengthOfPreviousField += description.frame.size.height + margin;
 
     [self.scrollView addSubview:description];
     
 //title
     
-    UILabel *ingredientsTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin, self.lengthOfPreviousFeild, widthWithMargin, 20)];
+    UILabel *ingredientsTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin, self.lengthOfPreviousField, widthWithMargin, 20)];
     ingredientsTitleLabel.text = @"Ingredients";
     self.view.backgroundColor = [UIColor whiteColor];
     [ingredientsTitleLabel sizeToFit];
-    self.lengthOfPreviousFeild += ingredientsTitleLabel.frame.size.height;
+    self.lengthOfPreviousField += ingredientsTitleLabel.frame.size.height;
     [self.scrollView addSubview:ingredientsTitleLabel];
 
     
@@ -62,42 +62,42 @@ static NSString * const ingredientVolumeKey = @"ingredientVolume";
     NSArray * ingredients = [RARecipes ingredientArrayAtIndex:self.indexPathSelected.row];
     for (int i = 0; i < ingredients.count; i++)
     {
-        UILabel *quantityLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin, self.lengthOfPreviousFeild, widthWithMargin, 20)];
+        UILabel *quantityLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin, self.lengthOfPreviousField, widthWithMargin, 20)];
         quantityLabel.text = ingredients[i][ingredientVolumeKey];
         [quantityLabel sizeToFit];
         CGFloat quantityHeight = quantityLabel.frame.size.height;
         [self.scrollView addSubview:quantityLabel];
         
-        UILabel *ingredientLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin + (widthWithMargin * .25) , self.lengthOfPreviousFeild, widthWithMargin * .75, 20)];
+        UILabel *ingredientLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin + (widthWithMargin * .25) , self.lengthOfPreviousField, widthWithMargin * .75, 20)];
         ingredientLabel.numberOfLines = 0;
         ingredientLabel.text = ingredients[i][ingredientKey];
         [ingredientLabel sizeToFit];
         CGFloat ingredientHeight = ingredientLabel.frame.size.height;
         [self.scrollView addSubview:ingredientLabel];
         
-        //Determine which label is taller
+//Determine which label is taller
         if (ingredientHeight > quantityHeight)
         {
-            self.lengthOfPreviousFeild += ingredientHeight;
+            self.lengthOfPreviousField += ingredientHeight;
         }
         else
         {
-            self.lengthOfPreviousFeild += quantityHeight;
+            self.lengthOfPreviousField += quantityHeight;
         }
     }
     
-    UILabel *directionTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin, self.lengthOfPreviousFeild, widthWithMargin, 20)];
+    UILabel *directionTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin, self.lengthOfPreviousField, widthWithMargin, 20)];
     directionTitleLabel.text = @"Directions";
     [directionTitleLabel sizeToFit];
-    self.lengthOfPreviousFeild += directionTitleLabel.frame.size.height;
+    self.lengthOfPreviousField += directionTitleLabel.frame.size.height;
     [self.scrollView addSubview:directionTitleLabel];
     
 //directions
 
-    NSArray *directions = [RARecipes directionsAtIndex:self.indexPathSelected.row];
-    for (int i = 0; i < directions.count; i++)
+    NSArray *directionsArray = [RARecipes directionsAtIndex:self.indexPathSelected.row];
+    for (int i = 0; i < directionsArray.count; i++)
     {
-        UILabel *numberedListLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin, self.lengthOfPreviousFeild, widthWithMargin * .10, 20)];
+        UILabel *numberedListLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin, self.lengthOfPreviousField, widthWithMargin * .10, 20)];
         
         //i+1 to start ingredients direction numbering at 1
         
@@ -106,9 +106,9 @@ static NSString * const ingredientVolumeKey = @"ingredientVolume";
         CGFloat numberedListHeight = numberedListLabel.frame.size.height;
         [self.scrollView addSubview:numberedListLabel];
         
-        UILabel *directionTextLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin + (widthWithMargin* .10), self.lengthOfPreviousFeild, widthWithMargin * .90, 20)];
+        UILabel *directionTextLabel = [[UILabel alloc] initWithFrame:CGRectMake(margin + (widthWithMargin* .10), self.lengthOfPreviousField, widthWithMargin * .90, 20)];
         directionTextLabel.numberOfLines = 0;
-        directionTextLabel.text = directions[i];
+        directionTextLabel.text = directionsArray[i];
         [directionTextLabel sizeToFit];
         CGFloat directionTextHeight = directionTextLabel.frame.size.height;
         [self.scrollView addSubview:directionTextLabel];
@@ -117,15 +117,15 @@ static NSString * const ingredientVolumeKey = @"ingredientVolume";
         
         if (directionTextHeight > numberedListHeight)
         {
-            self.lengthOfPreviousFeild += directionTextHeight;
+            self.lengthOfPreviousField += directionTextHeight;
         }
         else
         {
-            self.lengthOfPreviousFeild += numberedListHeight;
+            self.lengthOfPreviousField += numberedListHeight;
         }
     }
     
-    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.lengthOfPreviousFeild);
+    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, self.lengthOfPreviousField);
 }
 
 
